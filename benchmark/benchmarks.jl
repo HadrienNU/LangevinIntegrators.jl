ea04367124a96de56351c12f6e2cb19876e1ac6c
Hadrien <hadrien.vroylandt@sorbonne-universite.fr>
2022-08-21 18:34:14 +0200

    benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -8,23 +8,46 @@ using Random
 # On peut tester aussi le calul des forces pour les différents moyen
 const SUITE = BenchmarkGroup()
 
-SUITE["utf8"] = BenchmarkGroup(["string", "unicode"])
-teststr = String(join(rand(MersenneTwister(1), 'a':'d', 10^4)))
-SUITE["utf8"]["replace"] = @benchmarkable replace($teststr, "a" => "b")
-SUITE["utf8"]["join"] = @benchmarkable join($teststr, $teststr)
-SUITE["utf8"]["plots"] = BenchmarkGroup()
+SUITE["trajectories"] = BenchmarkGroup(["string", "unicode"])
+force=ForceFromPotential("Harmonic")
+integrator=EM(force,1.0,0.001)
+params=LangevinParams()
+SUITE["trajectories"]["init_EM"] = @benchmarkable init_trajectory($integrator; params = $params)
+state=init_trajectory(integrator; params = params)
+SUITE["trajectories"]["run_EM"] = @benchmarkable run_trajectory!($state, $integrator; params = $params)
 
-SUITE["trigonometry"] = BenchmarkGroup(["math", "triangles"])
-SUITE["trigonometry"]["circular"] = BenchmarkGroup()
-for f in (sin, cos, tan)
-    for x in (0.0, pi)
-        SUITE["trigonometry"]["circular"][string(f), x] = @benchmarkable ($f)($x)
-    end
-end
+integrator=BBK(force, 1.0, 1.0, 1.0, 1e-3)
+state=init_trajectory(integrator; params = params)
+SUITE["trajectories"]["run_BBK"] = @benchmarkable run_trajectory!($state, $integrator; params = $params)
 
-SUITE["trigonometry"]["hyperbolic"] = BenchmarkGroup()
-for f in (sin, cos, tan)
-    for x in (0.0, pi)
-        SUITE["trigonometry"]["hyperbolic"][string(f), x] = @benchmarkable ($f)($x)
-    end
-end
+integrator=GJF(force, 1.0, 1.0, 1.0, 1e-3)
+state=init_trajectory(integrator; params = params)
+SUITE["trajectories"]["run_GJF"] = @benchmarkable run_trajectory!($state, $integrator; params = $params)
+
+integrator=ABOBA(force, 1.0, 1.0, 1.0, 1e-3)
+state=init_trajectory(integrator; params = params)
+SUITE["trajectories"]["run_ABOBA"] = @benchmarkable run_trajectory!($state, $integrator; params = $params)
+
+integrator=BAOAB(force, 1.0, 1.0, 1.0, 1e-3)
+state=init_trajectory(integrator; params = params)
+SUITE["trajectories"]["run_BAOAB"] = @benchmarkable run_trajectory!($state, $integrator; params = $params)
+
+integrator=Verlet(force, 1.0, 1e-3)
+state=init_trajectory(integrator; params = params)
+SUITE["trajectories"]["run_Verlet"] = @benchmarkable run_trajectory!($state, $integrator; params = $params)
+
+#
+# SUITE["trigonometry"] = BenchmarkGroup(["math", "triangles"])
+# SUITE["trigonometry"]["circular"] = BenchmarkGroup()
+# for f in (sin, cos, tan)
+#     for x in (0.0, pi)
+#         SUITE["trigonometry"]["circular"][string(f), x] = @benchmarkable ($f)($x)
+#     end
+# end
+#
+# SUITE["trigonometry"]["hyperbolic"] = BenchmarkGroup()
+# for f in (sin, cos, tan)
+#     for x in (0.0, pi)
+#         SUITE["trigonometry"]["hyperbolic"][string(f), x] = @benchmarkable ($f)($x)
+#     end
+# end
