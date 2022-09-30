cbc3e459aff19d398acb0ddb728818061be58a94
Hadrien <hadrien@langevin>
2022-09-30 18:54:05 +0200

    Plot theory curve

--- examples/velocity_autocorrelation.jl
+++ examples/velocity_autocorrelation.jl
@@ -11,19 +11,25 @@ using StatsBase
 let
     force=ForceFromPotential("Harmonic")
 
-    params=TrajsParams(n_steps = 10^5, n_trajs = 10, n_save_iters = 1)
-    plot()
+    params=TrajsParams(n_steps = 5*10^5, n_trajs = 2, n_save_iters = 1)
+    Δt = 1e-4
+    tps = LinRange(0,div(params.n_steps,2)*Δt,div(params.n_steps,2)+1)
+    γ = 1.0
+    β = 1.0
+    ω = sqrt(1-γ^2/4)
+    inspectdr()
+    plot(tps,exp.(-0.5*γ*tps).*(cos.(ω*tps)-0.5*γ/ω*sin.(ω*tps))/β, label="Theory")
     for int_class in [ABOBA,BAOAB,BBK,GJF]
         println(String(Symbol(int_class)))
-        integrator=int_class(force, 1.0 , 1.0, 1.0, 1e-3, 1)
+        integrator=int_class(force, β , γ, 1.0, Δt, 1)
 
         trajs=run_trajectories(integrator; params = params, save_velocity=true)
         #Compute velocity auto correlation and plot it
         corr=zeros(div(params.n_steps,2)+1)
         for trj in trajs
-            corr.+=autocov(trj.vt,0:div(params.n_steps,2))[:,1]/params.n_trajs
+            corr.+=autocov(trj.vt[10^3:end],0:div(params.n_steps,2))[:,1]/params.n_trajs
         end
-        tps = LinRange(0,div(params.n_steps,2)*1e-3,div(params.n_steps,2)+1)
+
         plot!(tps, corr,label=String(Symbol(int_class)))
     end
     xlabel!("t")
