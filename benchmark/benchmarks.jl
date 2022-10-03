019043440b967917a536385811945fe576ae3bde
Hadrien <hadrien@langevin>
2022-10-03 17:56:36 +0200

    benchmark with kernel integrator

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -10,11 +10,13 @@ benchmarkdir=abspath(dirname(@__FILE__ ))
 init_conds_args=Dict("position"=> Dict("type"=>"Cste"),"velocity"=> Dict("type"=>"Cste"))
 force=ForceFromPotential("Harmonic")
 params=TrajsParams(;n_steps = 10^5)
+β = 1.0
+Δt = 1e-3
 
-integrator_em=EM(force,1.0,0.001)
+integrator_em=EM(force,β,0.001)
 init_conds_em=initialize_initcond(integrator_em,init_conds_args)
 
-integrator_bbk=BBK(force, 1.0, 1.0, 1.0, 1e-3)
+integrator_bbk=BBK(force, β, 1.0, 1.0, Δt)
 
 const SUITE = BenchmarkGroup()
 
@@ -23,31 +25,36 @@ SUITE["init"] = BenchmarkGroup(["initialization"])
 SUITE["init"]["overdamped"] = @benchmarkable generate_initial_conditions($integrator_em; params = $params,init_conds_args=$init_conds_args)
 SUITE["init"]["underdamped"] = @benchmarkable generate_initial_conditions($integrator_bbk; params = $params,init_conds_args=$init_conds_args)
 
-SUITE["integrator"] = BenchmarkGroup(["run"])
+SUITE["integrator_overdamped"] = BenchmarkGroup(["run"])
 
 state_em=InitState(integrator_em, init_conds_em)
-SUITE["integrator"]["run_EM"] = @benchmarkable run_trajectory!($state_em, $integrator_em; params = $params)
+SUITE["integrator_overdamped"]["run_EM"] = @benchmarkable run_trajectory!($state_em, $integrator_em; params = $params)
+
+
+SUITE["integrator_inertial"] = BenchmarkGroup(["run"])
 
 init_conds=initialize_initcond(integrator_bbk,init_conds_args)
 state_bbk=InitState(integrator_bbk, init_conds)
-SUITE["integrator"]["run_BBK"] = @benchmarkable run_trajectory!($state_bbk, $integrator_bbk; params = $params)
+SUITE["integrator_inertial"]["run_BBK"] = @benchmarkable run_trajectory!($state_bbk, $integrator_bbk; params = $params)
 
-integrator_gjf=GJF(force, 1.0, 1.0, 1.0, 1e-3)
+integrator_gjf=GJF(force, β, 1.0, 1.0, Δt)
 state_gjf=InitState(integrator_gjf, init_conds)
-SUITE["integrator"]["run_GJF"] = @benchmarkable run_trajectory!($state_gjf, $integrator_gjf; params = $params)
+SUITE["integrator_inertial"]["run_GJF"] = @benchmarkable run_trajectory!($state_gjf, $integrator_gjf; params = $params)
 
-integrator_aboba=ABOBA(force, 1.0, 1.0, 1.0, 1e-3)
+integrator_aboba=ABOBA(force, β, 1.0, 1.0, Δt)
 state_aboba=InitState(integrator_aboba, init_conds)
-SUITE["integrator"]["run_ABOBA"] = @benchmarkable run_trajectory!($state_aboba, $integrator_aboba; params = $params)
+SUITE["integrator_inertial"]["run_ABOBA"] = @benchmarkable run_trajectory!($state_aboba, $integrator_aboba; params = $params)
 
-integrator_baoab=BAOAB(force, 1.0, 1.0, 1.0, 1e-3)
+integrator_baoab=BAOAB(force, β, 1.0, 1.0, Δt)
 state_baoab=InitState(integrator_baoab, init_conds)
-SUITE["integrator"]["run_BAOAB"] = @benchmarkable run_trajectory!($state_baoab, $integrator_baoab; params = $params)
+SUITE["integrator_inertial"]["run_BAOAB"] = @benchmarkable run_trajectory!($state_baoab, $integrator_baoab; params = $params)
 
-integrator_verlet=Verlet(force, 1.0, 1e-3)
+integrator_verlet=Verlet(force, β, Δt)
 state_verlet=InitState(integrator_verlet, init_conds)
-SUITE["integrator"]["run_Verlet"] = @benchmarkable run_trajectory!($state_verlet, $integrator_verlet; params = $params)
+SUITE["integrator_inertial"]["run_Verlet"] = @benchmarkable run_trajectory!($state_verlet, $integrator_verlet; params = $params)
 
+
+SUITE["integrator_hidden"] = BenchmarkGroup(["run"])
 
 params_full,init_conds_args_hidden=read_conf(joinpath(benchmarkdir, "hidden_comparison.ini"))
 
@@ -55,13 +62,32 @@ integrator_hidden=read_integrator_hidden_npz(joinpath(benchmarkdir, "coeffs_benc
 init_conds_hidden=initialize_initcond(integrator_hidden,init_conds_args_hidden)
 state_hidden=InitState(integrator_hidden, init_conds_hidden)
 
-SUITE["integrator"]["run_EM_Hidden"] = @benchmarkable run_trajectory!($state_hidden, $integrator_hidden; params = $params)
+SUITE["integrator_hidden"]["run_EM_Hidden"] = @benchmarkable run_trajectory!($state_hidden, $integrator_hidden; params = $params)
 
 integrator_aboba_hidden=read_integrator_hidden_npz(joinpath(benchmarkdir, "coeffs_benchmark.npz"); integrator_type ="ABOBA", force=force)
 init_conds_hidden=initialize_initcond(integrator_aboba_hidden,init_conds_args_hidden)
 state_aboba_hidden=InitState(integrator_aboba_hidden, init_conds_hidden)
 
-SUITE["integrator"]["run_ABOBAHidden"] = @benchmarkable run_trajectory!($state_aboba_hidden, $integrator_aboba_hidden; params = $params)
+SUITE["integrator_hidden"]["run_ABOBAHidden"] = @benchmarkable run_trajectory!($state_aboba_hidden, $integrator_aboba_hidden; params = $params)
+
+SUITE["integrator_kernel"] = BenchmarkGroup(["run"])
+#Benchmark for Kernel integrator
+kernel= exp.(-20.0*LinRange(0,500*Δt, 500))
+integrator_kernelem=EM_Kernel(force, β , kernel, 1.0, Δt, 1)
+init_conds=initialize_initcond(integrator_kernelem,init_conds_args)
+state_kernelem=InitState(integrator_kernelem, init_conds)
+SUITE["integrator_kernel"]["run_EM"] = @benchmarkable run_trajectory!($state_kernelem, $integrator_kernelem; params = $params)
+
+integrator_kernelbbk=BBK_Kernel(force, β , kernel, 1.0, Δt, 1)
+init_conds=initialize_initcond(integrator_kernelbbk,init_conds_args)
+state_kernelbbk=InitState(integrator_kernelbbk, init_conds)
+SUITE["integrator_kernel"]["run_BBK"] = @benchmarkable run_trajectory!($state_kernelbbk, $integrator_kernelbbk; params = $params)
+
+integrator_kernelgjf=GJF_Kernel(force, β , kernel, 1.0, Δt, 1)
+init_conds=initialize_initcond(integrator_kernelgjf,init_conds_args)
+state_kernelgjf=InitState(integrator_kernelgjf, init_conds)
+SUITE["integrator_kernel"]["run_GJF"] = @benchmarkable run_trajectory!($state_kernelgjf, $integrator_kernelgjf; params = $params)
+
 
 
 # Benchmark with plumed
@@ -70,9 +96,9 @@ force_plumed=ForceFromPotential("Harmonic")
 addFix!(force_plumed, plumedFix)
 integrator_plumed=EM(force_plumed,1.0,integrator_em.Δt)
 state_em=InitState(integrator_plumed, init_conds_em)
-SUITE["integrator"]["run_EM_w_plumed"] = @benchmarkable run_trajectory!($state_em, $integrator_plumed; params = $params)
+SUITE["integrator_overdamped"]["run_EM_w_plumed"] = @benchmarkable run_trajectory!($state_em, $integrator_plumed; params = $params)
 params_x10=TrajsParams(;n_steps = 10^6)
-SUITE["integrator"]["run_EM_w_plumed_10x"] = @benchmarkable run_trajectory!($state_em, $integrator_plumed; params = $params_x10)
+SUITE["integrator_overdamped"]["run_EM_w_plumed_10x"] = @benchmarkable run_trajectory!($state_em, $integrator_plumed; params = $params_x10)
 # rm("plumed.log")
 
 
