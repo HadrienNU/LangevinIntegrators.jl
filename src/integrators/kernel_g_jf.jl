e3718d0d8f52b330864beffeb7e5ab52378884cf
Hadrien <hadrien@langevin>
2022-10-19 10:34:05 +0200

    add masses in fdt

--- src/integrators/kernel_g_jf.jl
+++ src/integrators/kernel_g_jf.jl
@@ -35,9 +35,9 @@ function GJF_Kernel(force::FP, β::TF, kernel::Array{TF}, M::TM, Δt::TF, dim::I
     a = (1 .- 0.5 * ker_mat[1,:,:] * Δt) *inv( (1 .+ 0.5 * ker_mat[1,:,:] * Δt))
     b = inv(1 .+ 0.5 * ker_mat[1,:,:] * Δt)
     if dim==1
-        noise_fdt=sqrt(Δt / β) * real.(ifft(sqrt.(fft(ker_mat,1)),1))
+        noise_fdt=sqrt(M * Δt / β) * real.(ifft(sqrt.(fft(ker_mat,1)),1))
     else
-        noise_fdt=sqrt(Δt / β) * real.(ifft(sqrt.(fft(ker_mat,1)),1))# note quand Kernel est une matrix il faut faire le cholesky
+        noise_fdt=sqrt(M * Δt / β) * real.(ifft(sqrt.(fft(ker_mat,1)),1))# note quand Kernel est une matrix il faut faire le cholesky
     end
     return GJF_Kernel(force, β, ker_mat, noise_fdt, M, Δt, a, b, dim, bc)
 end
