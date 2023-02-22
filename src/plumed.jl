b198596ce234d321877dba8f84e5e191e534e54f
Hadrien <hadrien.vroylandt@sorbonne-universite.fr>
2023-02-22 13:53:47 +0100

    removing conditionnal code

--- src/plumed.jl
+++ src/plumed.jl
@@ -4,7 +4,7 @@ module Plumed
     import LangevinIntegrators: AbstractFix, init_fix, apply_fix!, close_fix
     import LangevinIntegrators: AbstractIntegrator, addFix!
 
-    try
+    # try
         path_plumed = get(ENV,"PLUMED_INCLUDE_PATH","/usr/local/include")
 
         c`-llibplumed.so -I$(path_plumed)`
@@ -162,31 +162,31 @@ module Plumed
         end
 
 
-    catch e  # Allow to proceed even when plumed is not found
-        @warn("Unable to load plumed extension: ",e)
-        @warn("A empty plumed module have been loaded instead, it has not effect.")
-
-        struct plumed{TF<: AbstractFloat} <: AbstractFix
-                function plumed(plumed_input_file::String, plumed_log_file::String="p.log", dim::Int64=1, delta_t::Float64=1e-3; temperature=1.0)
-                    return new()
-                end
-        end
-
-        function init_fix(fix::plumed; kwargs...) # Différent du constructeur au sens que c'est appelé au début de chaque traj
-            @warn("Plumed has not been loaded, this is an empty fix.")
-            return fix
-        end
-
-        function apply_fix!(fix::plumed,x::Array{TF},f::Array{TF}; kwargs...) where {TF<:AbstractFloat}
-        	return false
-        end
-
-
-        function close_fix(fix::plumed)
-        end
-
-
-    end
+    # catch e  # Allow to proceed even when plumed is not found
+    #     @warn("Unable to load plumed extension: ",e)
+    #     @warn("A empty plumed module have been loaded instead, it has not effect.")
+    #
+    #     struct plumed{TF<: AbstractFloat} <: AbstractFix
+    #             function plumed(plumed_input_file::String, plumed_log_file::String="p.log", dim::Int64=1, delta_t::Float64=1e-3; temperature=1.0)
+    #                 return new()
+    #             end
+    #     end
+    #
+    #     function init_fix(fix::plumed; kwargs...) # Différent du constructeur au sens que c'est appelé au début de chaque traj
+    #         @warn("Plumed has not been loaded, this is an empty fix.")
+    #         return fix
+    #     end
+    #
+    #     function apply_fix!(fix::plumed,x::Array{TF},f::Array{TF}; kwargs...) where {TF<:AbstractFloat}
+    #     	return false
+    #     end
+    #
+    #
+    #     function close_fix(fix::plumed)
+    #     end
+    #
+    #
+    # end
 
     function addPlumed!(integrator::AbstractIntegrator,plumed_input_file::String, plumed_log_file::String="p.log", dim::Int64=1; temperature=1.0)
         addFix!(integrator, plumed(plumed_input_file, plumed_log_file, dim, integrator.Δt; temperature=temperature))
